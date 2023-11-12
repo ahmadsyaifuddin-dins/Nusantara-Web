e789252e8ed767cde72be0d666e10d87d3a7a3cc
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-11-12 11:11:46 +0800

    Update driver.js

--- driver.js
+++ driver.js
@@ -31,7 +31,7 @@ const driverObj = driver({
             }
         },
         {
-            element: '.home',
+            element: '.logo',
             popover: {
                 title: 'Selamat Berlihat-lihat',
                 description: 'Teruslah Hebat !',
