03eb0a7ac43b08f25260a46746faadf05ef9d979
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-11-12 19:43:51 +0800

    Update driver.js

--- driver.js
+++ driver.js
@@ -16,7 +16,7 @@ const driverObj = driver({
             element: '.form1',
             popover: {
                 title: 'Form Identitas',
-                description: 'Silahkan anda isi data anda di Form Identitas, Form ini sudah terintegrasi dengan API Formspree',
+                description: 'Silahkan anda isi data anda dengan Benar !!! di Form Identitas, Form ini sudah terintegrasi dengan Backend API Formspree',
                 side: "left",
                 align: 'start'
             }
@@ -25,7 +25,7 @@ const driverObj = driver({
             element: '.form2',
             popover: {
                 title: 'Form Pesan',
-                description: 'Silahkan anda berikan pesan kepada saya setelah anda meninjau dan mereview dari web ini di Form kontak ini, Form ini juga sudah terintegrasi dengan API Formspree',
+                description: 'Silahkan anda berikan pesan kepada saya setelah anda meninjau dan mereview dari web ini di Form kontak ini, Form ini juga sudah terintegrasi dengan Backend API Formspree',
                 side: "left",
                 align: 'start'
             }
