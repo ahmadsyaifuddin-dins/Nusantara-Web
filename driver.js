ea9efdb1509c058b98b1dc17c0d37b6418d7ff78
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2023-11-12 10:58:33 +0800

    Update driver.js

--- driver.js
+++ driver.js
@@ -3,7 +3,7 @@ const driver = window.driver.js.driver;
 const driverObj = driver();
 
 driverObj.highlight({
-    element: "#form",
+    element: ".form",
     popover: {
         title: "Form Identitas",
         description: "Silahkan anda isi data di Form Identitas, Form ini sudah terintegrasi dengan API Formspree"
